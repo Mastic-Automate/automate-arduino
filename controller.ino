032a8a52b6c71e2137442c6fe8623b7019accdae
YanCarlosCamargo <73352444+YanCarlosCamargo@users.noreply.github.com>
2022-06-20 22:39:10 -0300

    Adicionado sistema de turnos na rega
    A rega foi definida por turnos de giro do servo motor. Falta a medida da força da agua de acordo coma bomba.

--- controller.ino
+++ controller.ino
@@ -1,13 +1,15 @@
 #include <ArduinoJson.h>
-
+//Carrega a biblioteca Servo
+#include "Servo.h"
 #include <SoftwareSerial.h>
 #include <Wire.h>
 SoftwareSerial BT(10, 11); // TX, RX | TX para enviar dados e RX para receber dados. 
 String command = "";
-int rele = 0;
+int rele = 7;
 int button = 6;
 int val = 0;      // variável para guardar o valor lido
 String umidadeArduino;
+Servo servo1; 
 //String calor;
 StaticJsonBuffer<200> jsonBuffer;
 
@@ -19,13 +21,11 @@ void setUmidade(String umidade){
 
 }
 
-void executeCommand(String umidade, String arg){
+void executeCommand(String umidade, String arg, int times){
   if(umidade != umidadeArduino) {
-    Serial.println("Umidade alterada para: "+arg);
-  int bytesSent = BT.write("hello");
-  Serial.println(bytesSent);
+    Serial.println("Umidade alterada para: "+umidade);
+    servo(times);
    setUmidade(umidade);
-   
   }
 }
 
@@ -48,6 +48,40 @@ void writeString(String stringData){
   //BT.print("");
 }
 
+void regar(boolean val) {
+  if(val){
+  digitalWrite(rele, LOW);
+  } else {
+    digitalWrite(rele, HIGH);
+  }
+  }
+
+void servo(int times) {
+  for(times > 0; times--;){
+   
+    int i = 180;
+  // Repassa o angulo ao ServoWrite
+  
+  // Delay de 15ms para o Servo alcançar a posição
+ // delay(5000);
+servo1.write(180); 
+delay(1000); 
+ regar(true);
+  while (i > 0) {
+    //i=map(i, 0, 1023, 0, 180);
+  // Repassa o angulo ao ServoWrite
+  servo1.write(i); 
+  // Delay de 15ms para o Servo alcançar a posição
+  delay(15);
+  i--;
+  }
+  regar(false);
+  delay(500);
+}
+regar(false);
+  servo1.write(0); 
+}
+
 
 void setup()
 {
@@ -58,26 +92,31 @@ void setup()
   pinMode(rele, OUTPUT);  
   pinMode(button, INPUT);  
   pinMode(2, OUTPUT); //led que indica transmissão de dados para o celular 
-  pinMode(A5, INPUT);
- 
+  pinMode(A0, INPUT);
+  servo1.attach(5); 
+  digitalWrite(rele, HIGH);
 }
 
 void loop(){
-   val = analogRead(A5);
-  // if (calor > "500") {
-      //Serial.println(val);
-  //     BT.write("Muito Quente");
-  //     calor="300";
-   //}
-  
+
+
+servo1.write(0); 
+   
    if (Serial.available()){
        digitalWrite(2, HIGH);
       
-      String txt = Serial.readString();
-      writeString(txt);
+      String txt = String(Serial.readString());
+      //writeString(txt);
     
-     
-
+     JsonObject& root = jsonBuffer.parseObject(txt);
+     String umidade = root["umidade"];
+     String calor = root["calor"];
+     String vezes = root["vezes"];
+     int times = vezes.toInt();
+     executeCommand(umidade, calor,times);
+      Serial.println("Umidade recebida do Serial: "+umidade);
+    Serial.println("Calor recebido do Serial: "+calor);
+              
       // Serial.println(mensagem);
       
        delay(500);
@@ -92,23 +131,12 @@ void loop(){
      command = c;
      JsonObject& root = jsonBuffer.parseObject(c);
      String umidade = root["umidade"];
-    String calor = root["calor"];
-     //executeCommand(command, arg);
-       Serial.println(umidade);
-     Serial.println(command);
-     executeCommand(umidade, calor);
+     String calor = root["calor"];
+     String vezes = root["vezes"];
+     int times = vezes.toInt();
+     executeCommand(umidade, calor,times);
       Serial.println("Umidade recebida do celular: "+umidade);
     Serial.println("Calor recebido do celular: "+calor);
-    }
-   
-     
-
-        
-    if (command == "F"){
-      digitalWrite(rele,HIGH);
-    }
-    if(command == "B"){
-      digitalWrite(rele, LOW);
-    }
+    } 
  }
 }
