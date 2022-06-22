2b6b6dfb3e5b8f8a98065c77529de2681ad1135a
YanCarlosCamargo <73352444+YanCarlosCamargo@users.noreply.github.com>
2022-06-22 19:50:13 -0300

    implementando sensor de "humildade"
    Por enquanto comentado. já está funcional. pretendo incorporá-lo com mais tempo para trabalho.

--- controller.ino
+++ controller.ino
@@ -126,3 +126,37 @@ void loop(){
     } 
  }
 }
+
+/*
+const int pinoSensor = A0; //PINO UTILIZADO PELO SENSOR
+int valorLido; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
+ 
+int analogSoloSeco = 900; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+int analogSoloMolhado = 320; //VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
+int percSoloSeco = 0; //MENOR PERCENTUAL DO SOLO SECO (0% - NÃO ALTERAR)
+int percSoloMolhado = 100; //MAIOR PERCENTUAL DO SOLO MOLHADO (100% - NÃO ALTERAR)
+int valorSensor; 
+void setup(){
+ Serial.begin(9600); //INICIALIZA A SERIAL
+ Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
+ pinMode(4, OUTPUT);
+ delay(2000); //INTERVALO DE 2 SEGUNDOS
+}
+ 
+void loop(){  
+ valorSensor = analogRead(pinoSensor);
+ valorLido = constrain(valorSensor,analogSoloMolhado,analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
+ valorLido = map(valorLido,analogSoloMolhado,analogSoloSeco,percSoloMolhado,percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
+ Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
+ Serial.print(valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
+ Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
+ Serial.println(valorSensor);
+ if(valorLido > 70){
+  digitalWrite(4, HIGH);
+ } else {
+  digitalWrite(4, LOW);
+ }
+ delay(1000);  //INTERVALO DE 1 SEGUNDO
+}
+
+*/
