fb18e1168299a4a403b92c624f1485c6bc470caf
YanMCoutinho <yan.m.coutinho@gmail.com>
2022-08-10 18:18:08 -0300

    Added report function - working on it

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,166 +3,118 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
+int RELE = 7;
+int BUTTON = 6;
+int VAL = 0;      // variável para guardar o VALor lido
+int UMIDADEBD = 0;
+int DIASRELATORIO = 0;
+int RELATORIO[] = {0, 0};
+SoftwareSerial BT(10, 11); // TX, RX | TX para enviar dados e RX para receber dados.]
+Servo SERVO1; 
 
-
-SoftwareSerial BT(10, 11); // TX, RX | TX para enviar dados e RX para receber dados. 
-String command = "";
-int rele = 7;
-int button = 6;
-int val = 0;      // variável para guardar o valor lido
-String umidadeArduino;
-int vezes;
-String umidade;
-String calor;
-Servo servo1;
-DynamicJsonDocument jsonBuffer(1024); 
-//String calor;
-
-
-//function that sets the humidity
-void setUmidade(String umidade){
-  String log = "Umidade Antiga: "+umidadeArduino;
-  umidadeArduino = umidade;
-  log = "Umidade nova: "+umidadeArduino;
-}
-
-//Function that write on the phone with the bluetooth connection
-void writeString(String stringData){
-  BT.println(stringData);
-}
-
-//Function that check the humidity
-void executeCommand(String umidade, String arg, int times){
-  if(umidade != umidadeArduino) {
-    Serial.println("Umidade alterada para: "+umidade);
-  
-   setUmidade(umidade);
-  }
-    delay(500);
-    servo(times);
-    delay(500);
-   writeString("Agora foi. tenho certeza!!");
-    delay(3000);
-}
-
-
-//Function that irrigate
-void regar(boolean val) {
-  if(val){
-  digitalWrite(rele, LOW);
-  } else {
-    digitalWrite(rele, HIGH);
-  }
-}
+//BT.println(stringData);
 
 //Function that move the servomotor
-void servo(int times) {
+void rotateServo(int times, Servo servo) {
   for(times > 0; times--;){
-   
-    int i = 180;
-  // Repassa o angulo ao ServoWrite
-  
-  // Delay de 15ms para o Servo alcançar a posição
-  servo1.write(i); 
-  delay(500);
- digitalWrite(2,HIGH);
-
-//delay(100); 
- regar(true);
-  while (i > 0) {
-    //i=map(i, 0, 1023, 0, 180);
-  // Repassa o angulo ao ServoWrite
-  servo1.write(i); 
-  // Delay de 15ms para o Servo alcançar a posição
-  delay(7);
-  i--;
+    servo.write(180); 
+    delay(500);
+    servo.write(0); 
+    delay(500);
   }
-  regar(false);
-  delay(500);
-}
-//regar(false);
-  servo1.write(0); 
-  digitalWrite(2, LOW);
 }
 
+void regar(int times, Servo servo) {
+  digitalWrite(RELE, HIGH);
+  rotateServo(times, servo);
+  digitalWrite(RELE, LOW);
+}
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  Serial.println("Tipo de Comando realizado");
+  SERVO1.attach(5); 
   BT.begin(9600); // HC-05 usually default baud-rate
-  pinMode(rele, OUTPUT);  
-  pinMode(button, INPUT);  
+  pinMode(RELE, OUTPUT);  
+  //digitalWrite(RELE, HIGH);
+  pinMode(BUTTON, INPUT);  
   pinMode(2, OUTPUT); //led que indica transmissão de dados para o celular 
   pinMode(A0, INPUT);
-  servo1.attach(5); 
-  digitalWrite(rele, HIGH);
 }
 
 void loop(){
-   if (Serial.available()){
-       digitalWrite(2, HIGH);
-      BT.write(Serial.read());
-      
-      // delay(500);
-       digitalWrite(2, LOW);
-      }
-      
+  DynamicJsonDocument jsonDoc(1024);
+  
+  if (Serial.available()){
+    digitalWrite(2, HIGH);
+    BT.write(Serial.read());
+    digitalWrite(2, LOW);
+  }
   //Leitura da porta serial via bluetooth
   if (BT.available()){ //Caso aconteça alguma alteração na leitura da porta...
     while(BT.available()){
-     delay(15); 
-     Serial.read();
-     String c = String(BT.readString()); //Converte os dados recebidos em um caractere.
-     command = c;
-     DeserializationError error = deserializeJson(jsonBuffer, c);
-     if(!error) {
-       umidade = jsonBuffer["umidade"].as<String>();
-       vezes = jsonBuffer["vezes"];
-       calor = jsonBuffer["calor"].as<String>();
-       executeCommand(umidade,calor,vezes);
-       delay(1000);
-       Serial.println("Umidade recebida do celular: "+umidade);
-       Serial.println("Calor recebido do celular: "+calor); 
-     }
-     
+      
+      delay(15); 
+      Serial.read();
+      String json = String(BT.readString()); //Converte os dados recebidos em uma String.
+      DeserializationError error = deserializeJson(jsonDoc, json); //Descompacta o JSON recebido
+      
+      if(!error) {
+       bool dadosPlanta = jsonDoc["dadosPlanta"].as<bool>(); //Pega o atributo dadosPlanta do JSON
+       bool getRelatorio = jsonDoc["getRelatorio"].as<bool>(); //Pega o atributo dadosPlanta do JSON
+       
+       if (dadosPlanta){ //SE ESSE ATRIBUTO FOR TRUE, ELE RESETA O RELATÓRIO E COMEÇA A UTILIZAR ESSA NOVA MEDIDA DE UMIDADE
+        UMIDADEBD = jsonDoc["umidade"].as<int>();
+        //RESETA O RELATORIO PARA SEGUIR COM A NOVA UMIDADE
+        RELATORIO[0] = 0;
+        RELATORIO[1] = 0;
+        DIASRELATORIO = 0;
+        
+       } else if (getRelatorio) {
+        String response;
+        if (RELATORIO && UMIDADEBD) {
+          int mediaUmidade = RELATORIO[0] / RELATORIO[1];
+          jsonDoc["mediaUmidade"] = mediaUmidade;
+        } else {
+          jsonDoc["error"] = "Não há umidade inserida ou relatório suficiente para enviar um relatório";
+        }
+        serializeJson(jsonDoc, response);
+        BT.println(response);
+       }
+      }
     } 
- }
+  }
+
+  int umidade = 0;
+  if (UMIDADEBD) {
+    //IMPORTANTE IMPLEMENTAR PARA FUNCIONAR ↙
+    //umidade = LeituraDoSensorDeUmidade();
+    if (umidade < UMIDADEBD) {
+      regar(2, SERVO1);
+    }
+    float diasLigados = ((((millis() / 1000)/60)/60)/24); //Transforma milissegundos para segundos, depois para minutos, depois para horas e depois para dias
+    if (diasLigados > DIASRELATORIO) {
+      RELATORIO[0] = RELATORIO[0] + umidade;  
+      RELATORIO[1] = RELATORIO[1] + 1;
+      DIASRELATORIO = DIASRELATORIO + 1;
+    }
+    delay(5000);
+  }
 }
 
-/*
-const int pinoSensor = A0; //PINO UTILIZADO PELO SENSOR
-int valorLido; //VARIÁVEL QUE ARMAZENA O PERCENTUAL DE UMIDADE DO SOLO
- 
-int analogSoloSeco = 900; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
-int analogSoloMolhado = 320; //VALOR MEDIDO COM O SOLO MOLHADO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
-int percSoloSeco = 0; //MENOR PERCENTUAL DO SOLO SECO (0% - NÃO ALTERAR)
-int percSoloMolhado = 100; //MAIOR PERCENTUAL DO SOLO MOLHADO (100% - NÃO ALTERAR)
-int valorSensor; 
-void setup(){
- Serial.begin(9600); //INICIALIZA A SERIAL
- Serial.println("Lendo a umidade do solo..."); //IMPRIME O TEXTO NO MONITOR SERIAL
- pinMode(4, OUTPUT);
- delay(2000); //INTERVALO DE 2 SEGUNDOS
+/**
+MODELO DE JSON RECEBIDO:
+  PARA RECEBER O RELATORIO:
+{
+  "getRelatorio": true,
+  "dadosPlanta": false,
+  "umidade": 0,
 }
- 
-void loop(){  
- valorSensor = analogRead(pinoSensor);
- valorLido = constrain(valorSensor,analogSoloMolhado,analogSoloSeco); //MANTÉM valorLido DENTRO DO INTERVALO (ENTRE analogSoloMolhado E analogSoloSeco)
- valorLido = map(valorLido,analogSoloMolhado,analogSoloSeco,percSoloMolhado,percSoloSeco); //EXECUTA A FUNÇÃO "map" DE ACORDO COM OS PARÂMETROS PASSADOS
- int ml = (1024 - valorSensor)/70;
- Serial.print("Umidade do solo: "); //IMPRIME O TEXTO NO MONITOR SERIAL
- Serial.print(valorLido); //IMPRIME NO MONITOR SERIAL O PERCENTUAL DE UMIDADE DO SOLO
- Serial.println("%"); //IMPRIME O CARACTERE NO MONITOR SERIAL
- Serial.println(valorSensor);
-  Serial.println("ml:");
- Serial.println(ml);
- if(valorLido > 70){
-  digitalWrite(4, HIGH);
- } else {
-  digitalWrite(4, LOW);
- }
- delay(1000);  //INTERVALO DE 1 SEGUNDO
+  PARA INSERIR A UMIDADE:
+{
+  "getRelatorio": false,
+  "dadosPlanta": true,
+  "umidade": 324,
 }
 
+O ARDUINO SÓ PODE EXECUTAR UM POR VEZ, NÃO TEM COMO RECCEBER O RELATÓRIO E, AO MESMO TEMPO, ENVIAR OS DADOS DA PLANTA
 */
