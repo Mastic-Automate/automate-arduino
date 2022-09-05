0988e23f1e6c6db93345fa7b3de68698d00b01d4
YanMCoutinho <yan.m.coutinho@gmail.com>
2022-09-04 21:17:10 -0300

    Added parallel programming

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,5 +1,6 @@
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
+#include "ctimer.h"
 #include <Wire.h>
 
 int DRY_SOIL = 1023; //VALOR MEDIDO COM O SOLO SECO (VOCÊ PODE FAZER TESTES E AJUSTAR ESTE VALOR)
@@ -12,17 +13,17 @@ int BUTTON = 6;
 int VAL = 0;      // variável para guardar o VALor lido
 int HUMIDITYBD = 0;
 float REPORTDAYS = 0;
-const int CAPACITY = JSON_OBJECT_SIZE(18);
 int REPORT[] = {0, 0, 0};
+const int CAPACITY = JSON_OBJECT_SIZE(18);
 SoftwareSerial BT(10, 11); // TX, RX | TX para enviar dados e RX para receber dados.]
-
-void irrigate(int times) {
-  digitalWrite(WATER_BOMB, 255);
-  delay(times);
-  digitalWrite(WATER_BOMB, LOW);
-}
+cTimer dayTimer(true);
+cTimer irrigateTimer(false);
+cTimer drainTimer(true);
 
 void setup() {
+  dayTimer.SetTimeOut(86400000); //Número de milissegundos que tem em um dia
+  irrigateTimer.SetTimeOut(1000);
+  drainTimer.SetTimeOut(5000);
   Serial.begin(9600);
   BT.begin(9600); // HC-05 usually default baud-rate
   pinMode(WATER_BOMB, OUTPUT);  
@@ -38,7 +39,6 @@ void loop(){
   if (BT.available()){ //Caso aconteça alguma alteração na leitura da porta...
     while(BT.available()){
       Serial.read();
-      delay(15);
       String json = String(BT.readString()); //Converte os dados recebidos em uma String.
       DeserializationError error = deserializeJson(jsonDoc, json); //Descompacta o JSON recebido
       Serial.println("Erro em baixo: ");
@@ -83,19 +83,24 @@ void loop(){
   if (HUMIDITYBD) {
     //IMPORTANTE IMPLEMENTAR FUNÇÃO DE LER UMIDADE PARA FUNCIONAR ↙
     int sensorRead = analogRead(A0);
-    float humidity = map(sensorRead,WET_SOIL,DRY_SOIL,ONE_HUNDRED_PERCENT,ZERO_PERCENT); // tá em porcentagem
-    Serial.println(humidity);
-    if (humidity < HUMIDITYBD) {
-      irrigate(3000);
-      REPORT[2] = REPORT[2] + 1;
+    float humidity = map(sensorRead,WET_SOIL,DRY_SOIL,ONE_HUNDRED_PERCENT,ZERO_PERCENT); // tá em porcentagem    
+
+    if (drainTimer.IsTimeOut() && !irrigateTimer.IsEnabled()) {
+      if (humidity < HUMIDITYBD) {
+        digitalWrite(WATER_BOMB, HIGH);
+        irrigateTimer.Enable(true);  
+        irrigateTimer.IsTimeOut(true);
+        REPORT[2] = REPORT[2] + 1;
+      }
     }
-    float secondsOn = millis() / 1000; 
-    float minutesOn = secondsOn / 60; 
-    float hoursOn = minutesOn / 60; 
-    float daysOn = hoursOn / 24; //Transforma milissegundos para segundos, depois para minutos, depois para horas e depois para dias
-    Serial.println("Dias de Reportados");
-    Serial.println(REPORTDAYS);
-    if (daysOn > REPORTDAYS) {
+
+    if (irrigateTimer.IsTimeOut(false) && irrigateTimer.IsEnabled()) {
+      digitalWrite(WATER_BOMB, LOW);
+      irrigateTimer.Enable(false);  
+      drainTimer.IsTimeOut(true);
+    }
+    
+    if (!REPORTDAYS || dayTimer.IsTimeOut(true)) {
       REPORT[1] = REPORT[1] + 1;
       REPORT[0] = REPORT[0] + humidity;  
       REPORTDAYS = REPORTDAYS + 1;
